4dd03b4778f834b2b26779f274454a7e91ffef39
mhhar321 <38293743+mhhar321@users.noreply.github.com>
2019-04-18 07:32:04 -0700

    Add files via upload

---
+++ Plz Work/MovementScrip.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScrip : MonoBehaviour {
+    float speed = 2;
+    float rotSpeed = 40;
+    float rot = 0f;
+    float gravity = 8;
+    Vector3 moveDir = Vector3.zero;
+    CharacterController controller;
+    Animator anim;
+    public GameObject item;
+    public GameObject guide;
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();        
+        
+    }
+	
+	
+	void Update () {
+		if(controller.isGrounded)
+        {
+            if (Input.GetMouseButton(0) && item.transform.IsChildOf(guide.transform))
+            {
+                anim.SetInteger("condition", 4);
+                if (Input.GetKey(KeyCode.W))
+                {
+
+                    moveDir = new Vector3(0, 0, 1);
+                    moveDir = moveDir * speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+
+                    moveDir = new Vector3(0, 0, -1);
+                    moveDir = moveDir * speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.S))
+                {
+
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+
+                    moveDir = new Vector3(1, 0, 0);
+                    moveDir = moveDir * speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.D))
+                {
+
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+
+                    moveDir = new Vector3(-2, 0, 0);
+                    moveDir = moveDir * (speed / 2);
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.A))
+                {
+
+                    moveDir = new Vector3(0, 0, 0);
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                anim.SetInteger("condition", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    anim.SetInteger("condition", 1);
+                    moveDir = new Vector3(0, 0, 1);
+                    moveDir = moveDir * speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.W))             
+                {
+                    anim.SetInteger("condition", 0);
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    anim.SetInteger("condition", -1);
+                    moveDir = new Vector3(0, 0, -1);
+                    moveDir = moveDir * speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.S))
+                {
+                    anim.SetInteger("condition", 0);
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    anim.SetInteger("condition", 2);
+                    moveDir = new Vector3(1, 0, 0);
+                    moveDir = moveDir * speed;
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.D))
+                {
+                    anim.SetInteger("condition", 0);
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    anim.SetInteger("condition", 1);
+                    moveDir = new Vector3(-2, 0, 0);
+                    moveDir = moveDir * (speed / 2);
+                    moveDir = transform.TransformDirection(moveDir);
+                }
+                if (Input.GetKeyUp(KeyCode.A))
+                {
+                    anim.SetInteger("condition", 0);
+                    moveDir = new Vector3(0, 0, 0);
+                }
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    if (anim.GetInteger("condition") == 0)
+                    {
+                        anim.SetInteger("condition", -3);
+                    }
+                    else
+                    {
+                        anim.SetInteger("condtion", 0);
+                    }
+                }
+            }
+        }
+
+
+
+
+        moveDir.y -= gravity * Time.deltaTime;
+        float h = rotSpeed * Input.GetAxis("Mouse X");
+        float y = rotSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, h, 0);
+        controller.Move(moveDir * Time.deltaTime);
+     	   }
+}
---
+++ Plz Work/Pickup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;    
+    void Start()
+    {        
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float tempx = item.transform.position.x - tempParent.transform.position.x;
+        if(tempx < 0)
+        {
+            tempx *= -1;
+        }
+        
+        float tempz = item.transform.position.z - tempParent.transform.position.z;
+        if(tempz < 0)
+        {
+            tempz *= -1;
+        }
+        if (Input.GetMouseButton(0) && tempx < 0.5 && tempz < 1)
+        {
+            OnMouseDown();
+        }
+             else
+        {
+            OnMouseUp();
+        }
+        
+    }
+    private void OnMouseDown()
+    {
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = tempParent.transform;
+    
+}
+    private void OnMouseUp()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;        
+        item.transform.parent = null;
+        item.transform.rotation = item.transform.localRotation; 
+    }
+}
