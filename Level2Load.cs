98d7d59aae9f280527e2888acc65659c62c1799b
jwmas856 <49160846+jwmas856@users.noreply.github.com>
2019-04-18 08:03:00 -0700

    Add files via upload

---
+++ Level2Load.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Level2Load : MonoBehaviour {
+
+    private GameObject playerObj = null;
+    Bounds finishSquare;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (playerObj == null)
+            playerObj = GameObject.Find("Robot Kyle (1)");
+	}
+	
+    Vector3 GetStevePosition()
+    {
+        return playerObj.transform.position;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Debug.Log(GetStevePosition());
+
+        finishSquare = new Bounds(new Vector3(16.4f, 100.2f, 26.3f), new Vector3(5.0f,1.0f,5.0f));
+        if(finishSquare.Contains(GetStevePosition()))
+        {
+          SceneManager.LoadScene(2);
+        }
+    }
+}
---
+++ MainMenuButtonsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonsScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+    public void StartGame()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
